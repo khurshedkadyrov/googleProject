ac83b4b090af676b8ba8ea9b9ce6ca79a4d272aa
khursh.kadyrov <khursh.kadyrov@gmail.com>
2019-10-27 22:37:41 -0400

    New classes Chipmunk and Zoo created

---
+++ src/main/java/Chipmunk.java
@@ -0,0 +1,32 @@
+public class Chipmunk {
+    String name;
+    int age;
+    double weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+        public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+
+    }
